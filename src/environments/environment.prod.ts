ce1f47023f7eaac2337b96d4db2792c119bd49b7
iamrames <addictedtomig@gmail.com>
2019-07-12 15:59:43 +0545

    Added Production files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCMlLnXBN1Moq898FHRI8GKCW-Ie5_G7S0',
+    authDomain: 'basefire-demo.firebaseapp.com',
+    databaseURL: 'https://basefire-demo.firebaseio.com',
+    projectId: 'basefire-demo',
+    storageBucket: 'basefire-demo.appspot.com',
+    messagingSenderId: '788970455741',
+    appId: '1:788970455741:web:6125630a1235263e'
+  }
 };
